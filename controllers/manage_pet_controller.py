35e5ef679867c6236650b7d3b8ed2638dcc0051f
duyphong204 <dinhnguyenduyphong24@gmail.com>
2025-03-29 01:27:42 +0700

    update controller

--- controllers/manage_pet_controller.py
+++ controllers/manage_pet_controller.py
@@ -9,17 +9,13 @@ class ManagePetController:
         return self.pet_model.get_all_pets()
 
     def add_pet(self, pet_data):
-        # Thêm logic kiểm tra bổ sung nếu cần
-        self.pet_model.add_pet(pet_data)
+        return self.pet_model.add_pet(pet_data)
 
     def delete_pet(self, pet_id):
-        self.pet_model.delete_pet(pet_id)
+        return self.pet_model.delete_pet(pet_id)
 
-    def update_pet(self, pet_data):
-        self.pet_model.update_pet(pet_data)
+    def update_pet(self, pet_id, pet_data):
+        return self.pet_model.update_pet(pet_id, pet_data)
 
-    def search_pets(self, keyword, field):
-        return self.pet_model.search_pets(keyword, field)
-
-    def delete_all_pets(self):
-        self.pet_model.delete_all_pets()
+    def search_pets(self, keyword):
+        return self.pet_model.search_pets(keyword)
